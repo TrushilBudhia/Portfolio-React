de972f210eaaa89408d09e974d54395867f51006
Trushil Budhia <trushil.budhia@gmail.com>
2021-07-24 18:51:24 +0800

    Updated About.js and styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAt, faAddressCard } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faLinkedIn } from '@fortawesome/free-brands-svg-icons'
 import '../../styles/About.css'
 import '../../styles/style.css'
 import 'bulma/css/bulma.css'
@@ -10,24 +12,24 @@ export default function About() {
     <section id="about-me-section">
         <h2 className="is-size-2">About Me</h2>
         <p>Hi I am Trushil. A web developer who enjoys learning about advances in development technologies and building user friendly applications. I am based in Perth, Australia</p>
-        <div>Image<img className="photo" src={Photo} alt="Trushil Profile" /></div>
+        <img className="photo" src={Photo} alt="Trushil Profile" width="100" height="50" />
 
         <article id="contact-links">
             <a href="https://github.com/TrushilBudhia/" target="_blank" rel="noreferrer" className="btn contact-details">
-                <FontAwesomeIcon icon={['fab', 'github']} />
-                GitHub
+                <FontAwesomeIcon icon={faGithub} />
+                {` GitHub`}
             </a>
             <a href="https://www.linkedin.com/in/trushilbudhia/" target="_blank" rel="noreferrer" className="btn contact-details">
-                <FontAwesomeIcon icon={['fab', 'linkedin']} />
-                Linkedin
+                <FontAwesomeIcon icon={faLinkedin} />
+                {` Linkedin`}
             </a>
             <a href="mailto:trushil.budhia@gmail.com" target="_blank" rel="noreferrer" className="btn contact-details">
-                <FontAwesomeIcon icon={['fas', 'at']} />
-                Send an email
+                <FontAwesomeIcon icon={faAt} />
+                {` Send an email`}
             </a>
             <a href="./assets/documents/Trushil-Budhia-Resume.pdf" target="_blank" rel="noreferrer" className="btn contact-details">
-                <FontAwesomeIcon icon={['fas', 'address-card']} />
-                Resume
+                <FontAwesomeIcon icon={faAddressCard} />
+                {` Resume`}
             </a>
         </article>
     </section>
