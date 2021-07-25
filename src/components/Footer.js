de25127a7e21dff9e99a788fe6f2f7138feae908
Trushil Budhia <trushil.budhia@gmail.com>
2021-07-25 20:46:22 +0800

    Updated Resume.js and Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import '../styles/Footer.css'
 
 export default function Footer() {
     return (
         <footer>
-            <p>© Created by <a href="https://github.com/TrushilBudhia">Trushil</a></p>
+            <p>© Created by Trushil</p>
+            <a href="https://github.com/TrushilBudhia/" target="_blank" rel="noreferrer" className="footer-link has-text-white mx-3">
+                {/* Using the FontAwesomeIcon component imported to display the specified icon */}
+                <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href="https://www.linkedin.com/in/trushilbudhia/" target="_blank" rel="noreferrer" className="footer-link has-text-white mr-3">
+                <FontAwesomeIcon icon={faLinkedin} />
+            </a>
         </footer>
     );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
+import { SiReact, SiRedux, SiJavascript, SiJquery, SiWebpack, SiBootstrap, SiTailwindcss, SiBulma, SiCss3, SiHtml5, SiPhp, SiNodeDotJs, SiMysql, SiMongodb, SiGraphql } from "react-icons/si";
+
 import '../../styles/Resume.css';
 
 export default function Resume() {
@@ -15,14 +17,27 @@ export default function Resume() {
                 <hr className="horizontal-line" />
                 <article className="skills">
                     <h3 className="is-size-3 has-text-white">Proficiencies</h3>
-                    <article className="list">
-                        <article className="front-end is-block">
-                            <h4 className="is-size-3 has-text-white">Front-End</h4>
+                    <article className="list columns is-12 is-flex-wrap-wrap">
+                        <article className="front-end is-block columns is-half mx-3 my-3">
+                            <h4 className="is-size-3 has-text-white my-3">Front-End</h4>
+                            <div className="is-size-1 has-text-white">
+                            <SiReact className="mx-2" />
+                            <SiRedux className="mx-2" />
+                            <SiJavascript className="mx-2" />
+                            <SiJquery className="mx-2" />
+                            <SiWebpack className="mx-2" /><br />
+                            <SiBootstrap className="mx-2" />
+                            <SiTailwindcss className="mx-2" />
+                            <SiBulma className="mx-2" />
+                            <SiCss3 className="mx-2" />
+                            <SiHtml5 className="mx-2" />
+                            </div>
                             <ul>
                                 <li>React</li>
                                 <li>Redux</li>
                                 <li>JavaScript</li>
                                 <li>jQuery</li>
+                                <li>Webpack</li>
                                 <li>Responsive Design</li>
                                 <li>Bootstrap</li>
                                 <li>Tailwind CSS</li>
@@ -31,8 +46,15 @@ export default function Resume() {
                                 <li>HTML5</li>
                             </ul>
                         </article>
-                        <article className="back-end is-block">
-                            <h4 className="is-size-3 has-text-white">Back-End</h4>
+                        <article className="back-end is-block columns is-half mx-3 my-3">
+                            <h4 className="is-size-3 has-text-white my-3">Back-End</h4>
+                            <div className="is-size-1 has-text-white">
+                            <SiPhp className="mx-2" />
+                            <SiNodeDotJs className="mx-2" />
+                            <SiMysql className="mx-2" />
+                            <SiMongodb className="mx-2" />
+                            <SiGraphql className="mx-2" />
+                            </div>
                             <ul>
                                 <li>APIs</li>
                                 <li>PHP</li>
