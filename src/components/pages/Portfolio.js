3281d0073ec6272713834b0a3ac7f5ff5c4e4169
Trushil Budhia <trushil.budhia@gmail.com>
2021-07-25 19:47:33 +0800

    Updated Portfolio.js and Resume.js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,9 +2,23 @@ import React from 'react';
 import Project from '../Project.js'
 import '../../styles/Project.css';
 import EaseeAppointmentTrackerImg from '../../assets/images/Easee-Appointment-Tracker-2-img-1.jpg'
-import TechBlogImg from '../../assets/images/Tech-Blog-img-1.jpg'
-import EmployeeManagementSystemImg from '../../assets/images/Employee-Management-System-img-1.jpg'
-import NoteTakerImg from '../../assets/images/Note-Taker-img-1.jpg'
+// import BudgetTrackerPwaImg from '../../assets/images/Budget-Tracker-PWA-img-1.jpg'
+import FitnessTrackerImg from '../../assets/images/Fitness-Tracker-img-1.jpg'
+// import TechBlogImg from '../../assets/images/Tech-Blog-img-1.jpg'
+// import EmployeeManagementSystemImg from '../../assets/images/Employee-Management-System-img-1.jpg'
+// import NoteTakerImg from '../../assets/images/Note-Taker-img-1.jpg'
+import TeamProfileGeneratorImg from '../../assets/images/Team-Profile-Generator-img-1.jpg'
+// import WeatherDashboardImg from '../../assets/images/Weather-Dashboard-img-1.jpg'
+import CodeQuizImg from '../../assets/images/Code-Quiz-img-1.jpg'
+// import WorkDaySchedulerImg from '../../assets/images/Work-Day-Scheduler-img-1.jpg'
+import FlipCardImg from '../../assets/images/Flip-Card-img-1.jpg'
+import AutotyperImg from '../../assets/images/Autotyper-img-1.jpg'
+import BudgetTrackerPwaAnimatedImg from '../../assets/images/Budget-Tracker-PWA-img-2.gif'
+import TechBlogAnimatedImg from '../../assets/images/Tech-Blog-img-2.gif'
+import EmployeeManagementSystemAnimatedImg from '../../assets/images/Employee-Management-System-img-3.gif'
+import NoteTakerAnimatedImg from '../../assets/images/Note-Taker-img-2.gif'
+import WeatherDashboardAnimatedImg from '../../assets/images/Weather-Dashboard-img-2.gif'
+import WorkDaySchedulerAnimatedImg from '../../assets/images/Work-Day-Scheduler-img-2.gif'
 
 // This function builds the Portfolio section of the website - render each of the projects
 export default function Portfolio() {
@@ -20,32 +34,97 @@ export default function Portfolio() {
                         image={EaseeAppointmentTrackerImg} 
                         description="Easee Appointment Tracker preview"
                         title="Easee Appointment Tracker"
-                        technologies="Node.js / MySQL / Express / Sequelize / Javascript / Bulma CSS / Google Places API"
+                        technologies="Node.js / MySQL / Express / Sequelize / JavaScript / Bulma CSS / Google Places API"
                         repoLink="https://github.com/ShaneCurtis84/Easy-Appointment-Tracker-2.0"
                     />
                     <Project 
+                        siteLink="https://budget-tracker-pwa-plus.herokuapp.com/" 
+                        image={BudgetTrackerPwaAnimatedImg} 
+                        description="Budget Tracker PWA preview"
+                        title="Budget Tracker (PWA)"
+                        technologies="Node.js / MongoDB / Mongoose / Express / IndexedDB / JavaScript"
+                        repoLink="https://github.com/TrushilBudhia/Budget-Tracker-PWA"
+                    />
+                    <Project 
+                        siteLink="https://fitness-tracker-plus.herokuapp.com/" 
+                        image={FitnessTrackerImg} 
+                        description="Fitness Tracker preview"
+                        title="Fitness Tracker"
+                        technologies="Node.js / MongoDB / Mongoose / Express / JavaScript"
+                        repoLink="https://github.com/TrushilBudhia/Fitness-Tracker"
+                    />
+                    <Project 
                         siteLink="https://lets-get-technucated.herokuapp.com/" 
-                        image={TechBlogImg} 
+                        image={TechBlogAnimatedImg} 
                         description="Lets Get Technucated preview"
                         title="Tech Blog"
-                        technologies="Node.js / MySQL / Express / Sequelize / Javascript"
+                        technologies="Node.js / MySQL / Express / Sequelize / JavaScript"
                         repoLink="https://github.com/TrushilBudhia/Tech-Blog"
                     />
                     <Project 
                         siteLink="https://github.com/TrushilBudhia/Employee-Management-System#assets" 
-                        image={EmployeeManagementSystemImg} 
+                        image={EmployeeManagementSystemAnimatedImg} 
                         description="Employment Management System preview"
                         title="Employment Management System"
-                        technologies="Node.js / MySQL / Inquirer / Javascript"
+                        technologies="Node.js / MySQL / Inquirer / JavaScript"
                         repoLink="https://github.com/TrushilBudhia/Employee-Management-System"
                     />
                     <Project 
-                        siteLink="https://still-dusk-03326.herokuapp.com/" 
-                        image={NoteTakerImg} 
+                        siteLink="https://github.com/TrushilBudhia/Team-Profile-Generator#assets" 
+                        image={NoteTakerAnimatedImg} 
                         description="Note Taker preview"
                         title="Note Taker"
-                        technologies="Node.js / Express / Javascript"
+                        technologies="Node.js / Express / JavaScript"
                         repoLink="https://github.com/TrushilBudhia/Note-Taker"
+                    />
+                    <Project 
+                        siteLink="https://still-dusk-03326.herokuapp.com/" 
+                        image={TeamProfileGeneratorImg} 
+                        description="Team Profile Generator preview"
+                        title="Team Profile Generator"
+                        technologies="Node.js / Jest / Inquirer / JavaScript / Tailwind CSS"
+                        repoLink="https://github.com/TrushilBudhia/Team-Profile-Generator"
+                    />
+
+                    <Project 
+                        siteLink="https://trushilbudhia.github.io/Weather-Dashboard/" 
+                        image={WeatherDashboardAnimatedImg} 
+                        description="Weather Dashboard preview"
+                        title="Weather Dashboard"
+                        technologies="JavaScript / Bulma CSS / Google Places API"
+                        repoLink="https://github.com/TrushilBudhia/Weather-Dashboard"
+                    />
+                    <Project 
+                        siteLink="https://trushilbudhia.github.io/Code-Quiz/" 
+                        image={CodeQuizImg} 
+                        description="Code Quiz preview"
+                        title="Code Quiz"
+                        technologies="JavaScript / HTML / CSS"
+                        repoLink="https://github.com/TrushilBudhia/Code-Quiz"
+                    />
+                    <Project 
+                        siteLink="https://trushilbudhia.github.io/Work-Day-Scheduler/" 
+                        image={WorkDaySchedulerAnimatedImg} 
+                        description="Work Day Scheduler preview"
+                        title="Work Day Scheduler"
+                        technologies="jQuery / JavaScript / HTML / CSS"
+                        repoLink="https://github.com/TrushilBudhia/Work-Day-Scheduler"
+                    />
+                    <Project 
+                        siteLink="https://trushilbudhia.github.io/FlipCards-Half/" 
+                        image={FlipCardImg} 
+                        description="Flip Card preview"
+                        title="Flip Card"
+                        technologies="JavaScript / HTML / CSS"
+                        repoLink="https://github.com/TrushilBudhia/FlipCards-Half"
+                    />
+                    <Project 
+                        siteLink="https://trushilbudhia.github.io/SearchAnimate/" 
+                        image={AutotyperImg} 
+                        description="Autotyper preview"
+                        title="Autotyper"
+                        technologies="JavaScript / HTML / CSS"
+                        repoLink="https://github.com/TrushilBudhia/SearchAnimate"
                     />
                 </article>
             </article>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,52 @@
 import React from 'react';
-// import '../../styles/Resume.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
+import '../../styles/Resume.css';
 
 export default function Resume() {
-
+    return (
+        <section id="resume">
+            <h2>Resume</h2>
+            <article id="document-info">
+                <a href="../../assets/documents/Trushil-Budhia-Resume.pdf" target="_blank" rel="noreferrer" className="document-link">
+                    <FontAwesomeIcon icon={faAddressCard} />
+                    {` Download my resume`}
+                </a>
+                <hr className="horizontal-line" />
+                <article className="skills">
+                    <h3 className="is-size-3 has-text-white">Proficiencies</h3>
+                    <article className="list">
+                        <article className="front-end is-block">
+                            <h4 className="is-size-3 has-text-white">Front-End</h4>
+                            <ul>
+                                <li>React</li>
+                                <li>Redux</li>
+                                <li>JavaScript</li>
+                                <li>jQuery</li>
+                                <li>Responsive Design</li>
+                                <li>Bootstrap</li>
+                                <li>Tailwind CSS</li>
+                                <li>Bulma</li>
+                                <li>CSS3</li>
+                                <li>HTML5</li>
+                            </ul>
+                        </article>
+                        <article className="back-end is-block">
+                            <h4 className="is-size-3 has-text-white">Back-End</h4>
+                            <ul>
+                                <li>APIs</li>
+                                <li>PHP</li>
+                                <li>Node</li>
+                                <li>Express</li>
+                                <li>MySQL, Sequelize</li>
+                                <li>MongoDB, Mongoose</li>
+                                <li>REST</li>
+                                <li>GraphQL</li>
+                            </ul>
+                        </article>
+                    </article>
+                </article>
+            </article>
+        </section>
+    )
 }
