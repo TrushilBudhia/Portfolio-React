f887a1a7d0adc2619f95b99993ed2ec127f78216
Trushil Budhia <trushil.budhia@gmail.com>
2021-07-26 14:22:44 +0800

    Updated Resume.js and stylesheets

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
-import { SiReact, SiRedux, SiJavascript, SiJquery, SiWebpack, SiBootstrap, SiTailwindcss, SiBulma, SiCss3, SiHtml5, SiPhp, SiNodeDotJs, SiMysql, SiMongodb, SiGraphql } from "react-icons/si";
+import { SiReact, SiRedux, SiJavascript, SiWordpress, SiJquery, SiBootstrap, SiTailwindcss, SiBulma, SiCss3, SiHtml5, SiPhp, SiLaravel, SiNodeDotJs, SiMysql, SiMongodb, SiGraphql, SiAdobephotoshop, SiXampp, SiWebpack, SiGithub, SiHeroku, SiPostman } from "react-icons/si";
 import ResumeDocument from '../../assets/documents/Trushil-Budhia-Resume.pdf'
 import '../../styles/Resume.css';
 
@@ -24,20 +24,23 @@ export default function Resume() {
                             <SiReact className="mx-2" />
                             <SiRedux className="mx-2" />
                             <SiJavascript className="mx-2" />
+                            <SiWordpress className="mx-2" />
                             <SiJquery className="mx-2" />
-                            <SiWebpack className="mx-2" /><br />
+                            <br />
                             <SiBootstrap className="mx-2" />
                             <SiTailwindcss className="mx-2" />
                             <SiBulma className="mx-2" />
                             <SiCss3 className="mx-2" />
                             <SiHtml5 className="mx-2" />
+                            <br />
+
                             </div>
                             <ul>
                                 <li>React</li>
                                 <li>Redux</li>
                                 <li>JavaScript</li>
+                                <li>WordPress</li>
                                 <li>jQuery</li>
-                                <li>Webpack</li>
                                 <li>Responsive Design</li>
                                 <li>Bootstrap</li>
                                 <li>Tailwind CSS</li>
@@ -46,24 +49,47 @@ export default function Resume() {
                                 <li>HTML5</li>
                             </ul>
                         </article>
-                        <article className="back-end is-block columns is-half mx-3 my-3">
+                        <article className="back-end is-block columns mx-3 my-3">
                             <h4 className="is-size-3 has-text-white my-3">Back-End</h4>
                             <div className="is-size-1 has-text-white">
                             <SiPhp className="mx-2" />
+                            <SiLaravel className="mx-2" />
                             <SiNodeDotJs className="mx-2" />
                             <SiMysql className="mx-2" />
                             <SiMongodb className="mx-2" />
+                            <br />
                             <SiGraphql className="mx-2" />
                             </div>
                             <ul>
-                                <li>APIs</li>
                                 <li>PHP</li>
+                                <li>Laravel</li>
+                                <li>APIs</li>
                                 <li>Node</li>
                                 <li>Express</li>
                                 <li>MySQL, Sequelize</li>
                                 <li>MongoDB, Mongoose</li>
                                 <li>REST</li>
                                 <li>GraphQL</li>
+                            </ul>
+                        </article>
+                        <article className="other is-block columns is-half mx-3 my-3">
+                            <h4 className="is-size-3 has-text-white my-3">Other</h4>
+                            <div className="is-size-1 has-text-white">
+                            <SiAdobephotoshop className="mx-2" />
+                            <SiXampp className="mx-2" />
+                            <SiWebpack className="mx-2" />
+                            <SiGithub className="mx-2" />
+                            <SiHeroku className="mx-2" />
+                            <br />
+                            <SiPostman className="mx-2" />
+                            </div>
+                            <ul>
+                                <li>Photoshop</li>
+                                <li>XAMPP</li>
+                                <li>Webpack</li>
+                                <li>GitHub</li>
+                                <li>Heroku</li>
+                                <li>Postman</li>
                             </ul>
                         </article>
                     </article>
