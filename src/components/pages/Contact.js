4087c0f80ebf567692af846078f67cb0c4622f78
Trushil Budhia <trushil.budhia@gmail.com>
2021-07-27 00:18:20 +0800

    Updated Contact.js - overhauled the handleSubmit and handleChange functions - used hooks now

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,97 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { useInput } from '../../hooks/useInput;  
 import { AiOutlineMail } from "react-icons/ai";
 import { FaUser } from "react-icons/fa";
 import '../../styles/Contact.css';
 
-const numberOfElementsShown = async (elementSelect, numberOfElements) => {
-    if (elementSelect.length > numberOfElements) {
-        for (let i = 0; i < elementSelect.length - numberOfElements; i++) {
-            elementSelect[i].remove();
-        }
-    }
-}
-
-function handleSubmit(event) {
-    event.preventDefault();
-    const nameSelect = document.querySelector('.name').value.trim();
-    const emailSelect = document.querySelector('.email').value.trim();
-    const messageSelect = document.querySelector('.message').value.trim();
-    const inputSelect = document.querySelectorAll('.input');
-    const textAreaSelect = document.querySelector('.textarea');
-    const successMessageSection = document.querySelector('.success-message');
-    successMessageSection.setAttribute('class', 'success-message notification is-success my-4 is-hidden');
-
-    const errorMessage = document.querySelector(".error");
-    numberOfElementsShown(errorMessage, 0);
-    let errorMessageListItem;
-    if (!nameSelect) {
-        errorMessageListItem ? errorMessageListItem = errorMessageListItem + `<li>• Name</li>` : errorMessageListItem = `<li>• Name</li>`;
-    }
-    if (!emailSelect) {
-        errorMessageListItem ? errorMessageListItem = errorMessageListItem + `<li>• Email</li>` : errorMessageListItem = `<li>• Email</li>`;
-    }
-    if (!messageSelect) {
-        errorMessageListItem ? errorMessageListItem = errorMessageListItem + `<li>• Message</li>` : errorMessageListItem = `<li>• Message</li>`;
-    }
-    console.log('errorMessageListItem', errorMessageListItem);
-
-    const errorMessageSection = document.querySelector('.error-message');
-    errorMessageSection.setAttribute('class', 'error-message notification is-danger my-4 is-block');
-    errorMessage.innerHTML = errorMessageListItem;
-    if (nameSelect && emailSelect && messageSelect) {
-        inputSelect.forEach((item) => {
-            item.value = '';
-        })
-        textAreaSelect.value = '';
-        errorMessageSection.setAttribute('class', 'error-message is-hidden');
-        successMessageSection.setAttribute('class', 'success-message notification has-text-black is-success my-4 is-block');
-    }
-}
-
 export default function Contact() {
+    // Using the useState hook to create the variables and supporting functions that can be used to change the state of the variable
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+    const [submitted, setSubmitted] = useState(false);
+    const [valid, setValid] = useState(false);
+
+    // Handles what happens when the input field value changes
+    // Whenever the value prop of an input is set to a variable, you have a Controlled Component
+    // This function allows for the value of the input field to change and not be set to always hold the value set
+    const handleChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            [event.target.name]: event.target.value,
+        }));
+    };
+
+    // Handles what happens on form submit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setValid(false);
+        if (values.name && values.email && values.message) {
+            // sets valid to true if all the input fields are valid
+            // Valid being true allows the success message to be displayed
+            setValid(true);
+        }
+        // Sets submitted to true
+        // Will display any error spans if there are input fields that do not have a value
+        setSubmitted(true);
+    }
+
     return (
         <section id="contact">
             <article className="form-container">
                 <h1 className=" has-text-white is-size-3 my-3">Contact</h1>
-                <form className="contact-form column is-four-fifths">
+                <form className="contact-form column is-four-fifths" onSubmit={handleSubmit}>
                     <div className="field">
                         <label className="label has-text-white is-size-5">Name</label>
                         <div className="control has-icons-left">
-                            <input className="name input" type="text" placeholder="Email Name" />
+                            <input
+                                className="name input"
+                                id="name"
+                                type="text"
+                                placeholder="Email Name"
+                                name="name"
+                                value={values.name}
+                                onChange={handleChange}
+                            />
                             <span className="icon is-small is-left">
+                                {/* // Using the React Icons component to display the specified icon */}
                                 <FaUser />
                             </span>
+                            {/* // Error message will be displayed when submitted is true and there is no value in the input field */}
+                            {submitted && !values.name && <span id="name-error" className="has-text-danger my-4 is-block">*Please enter a valid name</span>}
                         </div>
                     </div>
                     <div className="field">
                         <label className="label has-text-white is-size-5">Email</label>
                         <div className="control has-icons-left">
-                            <input className="email input" type="email" placeholder="Enter Email" />
+                            <input
+                                className="email input"
+                                id="email" type="email"
+                                placeholder="Enter Email"
+                                name="email"
+                                value={values.email}
+                                onChange={handleChange} />
                             <span className="icon is-small is-left">
                                 <AiOutlineMail />
                             </span>
+                            {/* // Error message will be displayed when submitted is true and there is no value in the input field */}
+                            {submitted && !values.email && <span id="email-error" className="has-text-danger my-4 is-block">*Please enter a valid email</span>}
                         </div>
                     </div>
 
                     <div className="field">
                         <label className="label has-text-white is-size-5">Message</label>
                         <div className="control">
-                            <textarea className="message textarea" placeholder="Enter Message"></textarea>
+                            <textarea
+                                className="message textarea"
+                                placeholder="Enter Message"
+                                name="message"
+                                onChange={handleChange}
+                                value={values.message} >
+                            </textarea>
+                            {/* // Error message will be displayed when submitted is true and there is no value in the input field */}
+                            {submitted && !values.message && <span id="message-error" className="has-text-danger my-4 is-block">*Please enter a message</span>}
                         </div>
                     </div>
 
                     <div className="field is-grouped">
                         <div className="control">
-                            <button className="button is-link px-6" onClick={handleSubmit}>Submit</button>
+                            <button className="button is-link px-6" type="Submit">Submit</button>
                         </div>
                     </div>
-                    <div className="error-message notification is-danger my-4 is-hidden">
-                        Enter details for:
-                        <ul className="error"></ul>
-                    </div>
-                    <div className="success-message notification is-success my-4 is-hidden">
+                    {/* // Success message will display when the vaid value is true which happens when the form is submitted and all the input fields have valid values */}
+                    {valid && <div className="success-message notification is-success my-4 is-block">
                         <p>Message Sent</p>
-                    </div>
+                    </div>}
                 </form>
             </article>
         </section>
